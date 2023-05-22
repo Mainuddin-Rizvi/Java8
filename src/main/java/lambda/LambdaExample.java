680ac80bcd86a45afd1e9642fb84a696d0c1baab
Mainuddin <web97development@gmail.com>
2023-05-22 20:28:25 +0530

    refernce variable assigning implementation of interface

--- src/main/java/lambda/LambdaExample.java
+++ src/main/java/lambda/LambdaExample.java
@@ -4,30 +4,17 @@ interface Shape{
      void draw();
 }
 
-class Rectangle implements Shape{
-
-    @Override
-    public void draw() {
-        System.out.println("Rectangle class draw() method");
-    }
-}
-
-class Square implements Shape{
-
-    @Override
-    public void draw() {
-        System.out.println("Square class draw() method");
-    }
-}
-class Circle implements Shape{
-
-    @Override
-    public void draw() {
-        System.out.println("Circle class draw() method");
-    }
-}
 public class LambdaExample {
     public static void main(String[] args) {
+        Shape rectangle = ()->System.out.println("Rectangle class draw() method");
+        rectangle.draw();
+
+        Shape circle = ()->System.out.println("Circle class draw() method");
+        circle.draw();
+
+        Shape square = ()->System.out.println("Square class draw() method");
+        square.draw();
+
 
     }
 }
@@ -35,3 +22,8 @@ public class LambdaExample {
 //the class based is old techniques its need to write more code.
 //need object to execute.
 //functional interface help to cut it down the code.
+
+//functional interface need one abstract method in interface.
+
+//lambda expression need reference variablew to assign implementation
+//than call abstract method of interface eg. draw()
