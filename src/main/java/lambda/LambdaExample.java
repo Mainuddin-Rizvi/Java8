24714073b90b5b634d3230c25498269d60ecc502
Mainuddin <web97development@gmail.com>
2023-05-22 20:31:06 +0530

    passing refernce variable lambda expression in argument of method

--- src/main/java/lambda/LambdaExample.java
+++ src/main/java/lambda/LambdaExample.java
@@ -7,15 +7,18 @@ interface Shape{
 public class LambdaExample {
     public static void main(String[] args) {
         Shape rectangle = ()->System.out.println("Rectangle class draw() method");
-        rectangle.draw();
+        print(rectangle);
 
         Shape circle = ()->System.out.println("Circle class draw() method");
-        circle.draw();
+        print(circle);
 
         Shape square = ()->System.out.println("Square class draw() method");
-        square.draw();
+        print(square);
 
 
+    }
+    public static void print(Shape shape){
+        shape.draw();
     }
 }
 
@@ -27,3 +30,7 @@ public class LambdaExample {
 
 //lambda expression need reference variablew to assign implementation
 //than call abstract method of interface eg. draw()
+
+// passing reference variable with lambda expression into method
+
+
