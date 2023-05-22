bd874bab1e0ef81abd3e391752b4287b66864e55
Mainuddin <web97development@gmail.com>
2023-05-22 20:34:02 +0530

    functional programming

--- src/main/java/lambda/LambdaExample.java
+++ src/main/java/lambda/LambdaExample.java
@@ -6,14 +6,9 @@ interface Shape{
 
 public class LambdaExample {
     public static void main(String[] args) {
-        Shape rectangle = ()->System.out.println("Rectangle class draw() method");
-        print(rectangle);
-
-        Shape circle = ()->System.out.println("Circle class draw() method");
-        print(circle);
-
-        Shape square = ()->System.out.println("Square class draw() method");
-        print(square);
+        print(()->System.out.println("Rectangle class draw() method"));
+        print(()->System.out.println("Circle class draw() method"));
+        print(()->System.out.println("Square class draw() method"));
 
 
     }
